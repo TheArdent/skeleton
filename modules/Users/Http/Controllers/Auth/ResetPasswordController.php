f7e03f727211ea4e5d3401c03aef6a3e3c9a6dd9
Artyom Vorotnikov <outblast22@gmail.com>
2018-04-09 22:03:01 +0300

    Fix password reset

--- modules/Users/Http/Controllers/Auth/ResetPasswordController.php
+++ modules/Users/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,6 +5,9 @@ namespace Modules\Users\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
+use Illuminate\Auth\Events\PasswordReset;
 
 class ResetPasswordController extends Controller
 {
@@ -43,6 +46,19 @@ class ResetPasswordController extends Controller
         return view('users::auth.passwords.reset')->with(
             ['token' => $token, 'email' => $request->email]
         );
+    }
+
+    protected function resetPassword($user, $password)
+    {
+        $user->password = $password;
+
+        $user->setRememberToken(Str::random(60));
+
+        $user->save();
+
+        event(new PasswordReset($user));
+
+        $this->guard()->login($user);
     }
 
     /**
